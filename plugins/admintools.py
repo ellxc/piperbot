327a9a850ce06123b764f98d3e01552cf12e0aef
root <elliotcarr@friedonions.net>
2015-02-11 14:15:23 -0500

    changed to use message.reply()

--- plugins/admintools.py
+++ plugins/admintools.py
@@ -9,7 +9,6 @@ class AdminTools:
     @command("connect", adminonly=True)
     def connect(self, message):
         match = self.connect_regex.match(message.text)
-        response = message.get_reply()
         if match:
             try:
                 name, network, port, channels = match.groups()
@@ -18,21 +17,18 @@ class AdminTools:
                 response.text = "connecting to {}!".format(name if name else network)
                 yield response
             except Exception as e:
-                response.text = str(e)
-                yield response
+                yield message.reply(str(e))
         else:
-            response.text = "malformed command!"
-            yield response
+            yield message.reply("malformed command!")
 
     @command("join", adminonly=True)
     def join(self, message):
-        response = message.get_reply()
-        response.text = ""
+        response = message.reply("")
         response.command = "JOIN"
         for channel in message.text.split():
             if channel and channel[0] in "#&!+":
                 response.params = channel
-                yield response.copy()
+                yield response
 
     @command("quit", adminonly=True)
     def quit(self, message):
@@ -43,74 +39,57 @@ class AdminTools:
 
     @command("leave", adminonly=True)
     def leave(self, message):
-        response = message.get_reply()
-        response.text = ""
+        response = message.reply("")
         response.command = "PART"
         if message.text.strip():
             for channel in message.text.split():
                 if channel and channel[0] in "#&!+":
                     response.params = channel
-                    yield response.copy()
+                    yield response
         else:
             yield response
 
     @command("load", adminonly=True)
     def load(self, message):
-        response = message.get_reply()
         if message.text.strip():
             for plugin in message.text.split():
                 try:
                     self.bot.load_plugin_from_module(plugin)
-                    response.text = "Loaded plugin(s) " + plugin
-                    yield response
+                    yield message.reply("Loaded plugin(s) " + plugin)
                 except Exception as e:
-                    response.text = "error: " + str(e)
-                    yield response
+                    yield message.reply("error: " + str(e))
 
     @command("unload", adminonly=True)
     def unload(self, message):
-        response = message.get_reply()
         if message.text.strip():
             for plugin in message.text.split():
                 try:
                     self.bot.unload_module(plugin)
-                    response.text = "Unloaded plugin(s) " + plugin
-                    yield response
+                    yield message.reply("Unloaded plugin(s) " + plugin)
                 except Exception as e:
-                    response.text = "error: " + str(e)
-                    yield response
+                    yield message.reply("error: " + str(e))
 
     @command("reload", adminonly=True)
     def reload(self, message):
-        response = message.get_reply()
         if message.text.strip():
             for plugin in message.text.split():
                 try:
                     self.bot.unload_module(plugin)
                     self.bot.load_plugin_from_module(plugin)
-                    response.text = "reloaded plugin(s) " + plugin
-                    yield response
+                    yield message.reply("reloaded plugin(s) " + plugin)
                 except Exception as e:
-                    response.text = "error: " + str(e)
-                    yield response
+                    yield message.reply("error: " + str(e))
 
     @command("eval", adminonly=True)
     def eval(self, message):
-        response = message.get_reply()
         try:
-            response.text = str(eval(message.text))
-            yield response
+            yield message.reply(str(eval(message.text)))
         except Exception as e:
-            response.text = str(e)
-            yield response
+            yield message.reply(str(e))
 
     @command("exec", adminonly=True)
     def execer(self, message):
-        response = message.get_reply()
         try:
             exec(message.text)
-            #response.text = "done"
-            #yield response
         except Exception as e:
-            response.text = str(e)
-            yield response
+            yield message.reply(str(e))
